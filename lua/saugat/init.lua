9d64c1cf1138c18c6d952cba649c3a7f990862ca
Saugat Rai <saugat.rai@intimetec.com>
2024-07-26 12:07:49 +0530

    add telescope

--- lua/saugat/init.lua
+++ lua/saugat/init.lua
@@ -6,7 +6,8 @@ require("saugat.after.plugins.colorscheme")
 require("saugat.after.plugins.treesitter")
 require("saugat.after.plugins.lspzero")
 
-vim.wo.rnu = true
+vim.wo.relativenumber = true
+vim.wo.number = true
 vim.o.tabstop = 4
 vim.o.expandtab = false
 vim.o.softtabstop = 4
